e272a97ae618c840988d609d79f4c02738b2bf3c
crispo513 <haolun7788@gmail.com>
2025-06-29 22:39:59 -0700

    Integrated Vercel

---
+++ api/steam.js
@@ -0,0 +1,17 @@
+// /api/steam.js
+
+export default async function handler(req, res) {
+    const { steamid } = req.query;
+  
+    const apiKey = process.env.STEAM_API_KEY;
+    const url = `https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${apiKey}&steamids=${steamid}`;
+  
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      res.status(200).json(data);
+    } catch (err) {
+      res.status(500).json({ error: "Failed to fetch Steam data." });
+    }
+  }
+  
